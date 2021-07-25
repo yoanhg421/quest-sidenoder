a5d4e47dd93fd4d9b280e3159d7eca5fac056ac7
vKolerts <v.kolerts@gmail.com>
2021-07-25 19:33:51 +0300

    +

--- views/js/main.js
+++ views/js/main.js
@@ -1,6 +1,13 @@
 function id(el_id) {
   return document.getElementById(el_id);
 }
+
 function $id(el_id) {
   return $(id(el_id));
+}
+
+function copyInput(el) {
+  el.select();
+  document.execCommand('copy');
+  alert('Text copied to clipboard');
 }
